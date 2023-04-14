c2a334974ad8504bfbf1c4f2b7682aecbef50977
am3rco <am3rco@gmail.com>
2023-04-13 21:01:40 -0300

    Estilizando o Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <h1 className="title">Home</h1>
+      <p>
+        <img
+          src="https://i.pinimg.com/originals/ea/fe/a8/eafea8caa4d0f1bb85f424cd131c9436.jpg"
+          alt="Gato Vampiro Fofo"
+          className="img"
+        />
+      </p>
+    </>
+  );
+}
+
+export default Home;

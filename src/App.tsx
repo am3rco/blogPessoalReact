13ef5de155928cd9ddea6bc08315d3d4daf1e85c
am3rco <am3rco@gmail.com>
2023-04-17 14:50:08 -0300

    Implementação do BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import React from "react";
 import Home from "./paginas/home/Home";
+import Login from "./paginas/login/Login";
 import Navbar from "./components/estaticos/navbar/Navbar";
 import Footer from "./components/estaticos/footer/Footer";
 import "./App.css";
@@ -9,9 +10,17 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div style={{ minHeight: "100vh" }}>
+          <Routes>
+            <Route path="/" element={<Login />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }

90e37688beff4d6ccceadc9f4a1a3494ee47649f
am3rco <am3rco@gmail.com>
2023-04-17 14:29:23 -0300

    Estilizando o Home

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,10 +1,58 @@
 import React from "react";
 
 import "./Home.css";
+import { Button, Grid, Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
 function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        className="bg-home"
+      >
+        <Grid alignItems="center" item xs={6}>
+          <Box paddingX={20}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="h3"
+              align="center"
+              className="title-text"
+            >
+              Seja bem vindo(a)!
+            </Typography>
+            <Typography
+              variant="h5"
+              gutterBottom
+              color="textPrimary"
+              component="h5"
+              align="center"
+              className="title-text"
+            >
+              Expresse aqui os seus pensamentos e opiniões!
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="center">
+            <Box marginRight={1}></Box>
+            <Button variant="outlined" className="botao">
+              Ver Postagens
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src="https://i.imgur.com/H88yIo2.png"
+            alt=""
+            width="500px"
+            height="500px"
+          />
+        </Grid>
+        <Grid xs={12} className="postagens"></Grid>
+      </Grid>
     </>
   );
 }

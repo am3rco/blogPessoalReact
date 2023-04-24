6e63f57a41bef1848bdcfb15898c003b8fe37ba0
am3rco <am3rco@gmail.com>
2023-04-24 18:25:19 -0300

    Criando Componentes Postagens e Temas

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Navbar from "./components/estaticos/navbar/Navbar";
 import Footer from "./components/estaticos/footer/Footer";
 import "./App.css";
 import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
+import ListaTema from "./components/temas/listatema/ListaTema";
+import ListaPostagem from "./components/postagens/listapostagens/ListaPostagem";
 
 function App() {
   return (
@@ -18,10 +20,13 @@ function App() {
             <Route path="/" element={<Login />}></Route>
             <Route path="/home" element={<Home />}></Route>
             <Route path="/login" element={<Login />}></Route>
+
             <Route
               path="/cadastrousuario"
               element={<CadastroUsuario />}
             ></Route>
+            <Route path="/temas" element={<ListaTema />}></Route>
+            <Route path="/posts" element={<ListaPostagem />}></Route>
           </Routes>
         </div>
         <Footer />

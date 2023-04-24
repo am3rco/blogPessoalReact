032184d8ac9a324cdbea61140a64153d5fee3b03
am3rco <am3rco@gmail.com>
2023-04-24 20:03:43 -0300

    Comunicação com o Back-end Tema

--- src/components/temas/listatema/ListaTema.tsx
+++ src/components/temas/listatema/ListaTema.tsx
@@ -11,6 +11,7 @@ import Tema from "../../../models/Tema";
 import { Box } from "@mui/material";
 import "./ListaTema.css";
 import useLocalStorage from "react-use-localstorage";
+import { busca } from "../../../services/Service";
 
 function ListaTema() {
   const [temas, setTemas] = useState<Tema[]>([]);
@@ -23,43 +24,64 @@ function ListaTema() {
       navigate("/login");
     }
   }, [token]);
+
+  async function getTema() {
+    await busca("/tema", setTemas, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  useEffect(() => {
+    getTema();
+  }, [temas.length]);
+
   return (
     <>
-      <Box m={2}>
-        <Card variant="outlined">
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Tema
-            </Typography>
-            <Typography variant="h5" component="h2">
-              Minha descrição
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Box display="flex" justifyContent="center" mb={1.5}>
-              <Link to="" className="text-decorator-none">
-                <Box mx={1}>
-                  <Button
-                    variant="contained"
-                    className="marginLeft"
-                    size="small"
-                    color="primary"
-                  >
-                    atualizar
-                  </Button>
-                </Box>
-              </Link>
-              <Link to="" className="text-decorator-none">
-                <Box mx={1}>
-                  <Button variant="contained" size="small" color="secondary">
-                    deletar
-                  </Button>
-                </Box>
-              </Link>
-            </Box>
-          </CardActions>
-        </Card>
-      </Box>
+      {temas.map((tema) => (
+        <Box m={2}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                Tema
+              </Typography>
+              <Typography variant="h5" component="h2">
+                {tema.descricao}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Box display="flex" justifyContent="center" mb={1.5}>
+                <Link
+                  to={`/formularioTema/${tema.id}`}
+                  className="text-decorator-none"
+                >
+                  <Box mx={1}>
+                    <Button
+                      variant="contained"
+                      className="marginLeft"
+                      size="small"
+                      color="primary"
+                    >
+                      atualizar
+                    </Button>
+                  </Box>
+                </Link>
+                <Link
+                  to={`/deletarTema/${tema.id}`}
+                  className="text-decorator-none"
+                >
+                  <Box mx={1}>
+                    <Button variant="contained" size="small" color="secondary">
+                      deletar
+                    </Button>
+                  </Box>
+                </Link>
+              </Box>
+            </CardActions>
+          </Card>
+        </Box>
+      ))}
     </>
   );
 }
